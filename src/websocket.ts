ddc152ea9b833c298a8b0ecc603af17999df93b7
Samuel Simões <samuelsimoes31@gmail.com>
2021-09-11 15:51:29 -0300

    📦feat: testing P5.js

--- src/websocket.ts
+++ src/websocket.ts
@@ -1,5 +1,10 @@
 import { io } from './http';
 
 io.on('connection', socket => {
-  console.log(socket.id);
+  console.log(`-> ${socket.id} connected!`);
+
+  socket.on('disconnect',() => {
+    console.log(`<- ${socket.id} disconnected!`);
+  });
+
 });
