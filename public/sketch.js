716e8732a7a12638f27d9dddb411df48c546b439
Samuel Simões <samuelsimoes31@gmail.com>
2021-09-11 18:09:32 -0300

    📦feat: broadcasting square positions between clients

--- public/sketch.js
+++ public/sketch.js
@@ -2,16 +2,21 @@ const position = {x: 50, y: 50};
 
 function setup() {
   createCanvas(500, 500);
+  background(120,143,160);
+  rect(0,0,50,50);
+
+  socket.on('positions', positions => {
+    background(120,143,160);
+    for (const [id, position] of Object.entries(positions)){
+      rect(position.x,position.y,50,50);
+    }
+  })
 }
 
 function draw() {
-  background(120,143,160);
-  rect(position.x,position.y,50,50);
+
 }
 
 function keyPressed() {
-  if(key === 'w') position.y -= 50;
-  else if(key === 's') position.y += 50;
-  else if(key === 'a') position.x -= 50;
-  else if(key === 'd') position.x += 50;
+  socket.emit('keyboard',key);
 }
--- src/websocket.ts
+++ src/websocket.ts
@@ -1,10 +1,27 @@
 import { io } from './http';
 
+const positions: {[key: string]: {x: number,y: number}}  = {};
+
 io.on('connection', socket => {
-  console.log(`-> ${socket.id} connected!`);
+  const id = socket.id;
+  console.log(`-> ${id} connected!`);
+
+  positions[id] = {x:0,y:0};
+
+  socket.on('keyboard',key => {
+    console.log(`Key [${key}] from ${id}`);
+    const {x,y} = positions[id];
+    if(key === 'w') positions[id] = {x,y: y-50};
+    else if(key === 's') positions[id] = {x,y: y+50};
+    else if(key === 'a') positions[id] = {x:x-50,y};
+    else if(key === 'd') positions[id] = {x:x+50,y};
+
+    io.emit('positions',positions);
+  })
 
   socket.on('disconnect',() => {
-    console.log(`<- ${socket.id} disconnected!`);
+    console.log(`<- ${id} disconnected!`);
+    delete positions[id];
   });
 
 });
